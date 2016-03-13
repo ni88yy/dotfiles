485981546b8f0e86ea0f92c3a891ae4863acf69d
Sayre Blades <sayre.blades@gmail.com>
2016-03-13 19:40:59 -0400

    SAVEPOINT

--- .tmuxifier/layouts/marix-tv-tizen.session.sh
+++ .tmuxifier/layouts/marix-tv-tizen.session.sh
@@ -1,11 +1,11 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-session_root "~/dev/marix/marix-node/"
+session_root "~/dev/marix/marix-tv-tizen/"
 
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session "marix-node"; then
+if initialize_session "marix-tv-tizen"; then
 
   # Create a new window inline within session layout definition.
   new_window
